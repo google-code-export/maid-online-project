3783b9ed8f479198b92aa904c897facfc4c1e9c4
indorikoshin@gmail.com <indorikoshin@gmail.com@1fc84788-3244-a7dc-4ca5-77ddbb7c2022>
2012-01-02 12:41:45 +0000

    Issue 9 Issue 10 Issue 11 : class pesanan

---
+++ trunk/maid/src/java/entity/Pesanan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Ricko Shin
+ */
+public class Pesanan {
+    
+}
