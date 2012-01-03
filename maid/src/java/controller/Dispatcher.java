f915f8f6a591f5fc79db6fca4503ab32b8a516da
indorikoshin@gmail.com <indorikoshin@gmail.com@1fc84788-3244-a7dc-4ca5-77ddbb7c2022>
2012-01-03 04:41:34 +0000

    Issue 9 Issue 10  Issue 11 : Dispatcher untuk semua halaman

--- maid/src/java/controller/Dispatcher.java
+++ maid/src/java/controller/Dispatcher.java
@@ -1,8 +1,11 @@
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package controller;
 
-import entity.Pelanggan;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,43 +28,59 @@ public class Dispatcher extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
         String page = request.getParameter("page");
-         RequestDispatcher dis = null;
-        ControllerPelanggan controller = new ControllerPelanggan(request);
-        Pelanggan pelanggan = new Pelanggan();
-        if (page != null) { 
+        RequestDispatcher dis = null;
+
+        if (page != null) {
             if (page.equals("pesan")) {
-                controller.setRegistrasi(pelanggan);
-                dis = request.getRequestDispatcher("Pesan.jsp");
-            }
-       
-            if (page.equals("pagekoki")) {
-                  dis = request.getRequestDispatcher("indexkoki.jsp");
-                }
+                dis = request.getRequestDispatcher("pesan.jsp");
+            }else if (page.equals("loginpelanggan")) {
+                dis = request.getRequestDispatcher("loginpelanggan.jsp");
+            } else if (page.equals("pagekoki")) {
+                dis = request.getRequestDispatcher("indexkoki.jsp");
+            } else if (page.equals("pagekasir")) {
+                dis = request.getRequestDispatcher("indexkasir.jsp");
+            } else if (page.equals("halamankoki")) {
+                dis = request.getRequestDispatcher("halamankoki.jsp");
+            } else if (page.equals("halamankasir")) {
+                dis = request.getRequestDispatcher("halamankasir.jsp");
+            } else if (page.equals("halamankasirnota")) {
+                dis = request.getRequestDispatcher("halamankasirnota.jsp");
+            } else if (page.equals("bon_validasi")) {
+                dis = request.getRequestDispatcher("bon_validasi.jsp");
+            } else if (page.equals("terimakasih_telahpesan")) {
+                dis = request.getRequestDispatcher("terimakasih_telahpesan.jsp");
+            } else if (page.equals("index")) {
+                dis = request.getRequestDispatcher("index.jsp");
+        
             
-            if (page.equals("pagekasir")) {
-                  dis = request.getRequestDispatcher("indexkasir.jsp");
-                }
-       
-    } else {
-                    dis = request.getRequestDispatcher("index.jsp");
+         
+            }
+         else {
+            dis = request.getRequestDispatcher("index.jsp");
+
         }
+
         dis.include(request, response);
-        }
+
+      }
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-  @Override
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-  }
+        processRequest(request, response);
+    }
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
      * @param response servlet response
@@ -71,15 +90,16 @@ public class Dispatcher extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        }
-    
+        processRequest(request, response);
+    }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 }
+
