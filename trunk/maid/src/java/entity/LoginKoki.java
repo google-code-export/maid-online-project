8155cf8f48733ee1cc6a8cf9de0225496735a209
indorikoshin@gmail.com <indorikoshin@gmail.com@1fc84788-3244-a7dc-4ca5-77ddbb7c2022>
2012-01-12 05:59:48 +0000

    Issue 10 : penambahan entitas koki

--- trunk/maid/src/java/entity/LoginKoki.java
+++ trunk/maid/src/java/entity/LoginKoki.java
@@ -4,6 +4,7 @@
  */
 package entity;
 
+import controller.controllerLoginKoki;
 import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -14,6 +15,8 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -28,16 +31,31 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "LoginKoki.findById", query = "SELECT l FROM LoginKoki l WHERE l.id = :id"),
     @NamedQuery(name = "LoginKoki.findByPassword", query = "SELECT l FROM LoginKoki l WHERE l.password = :password")})
 public class LoginKoki implements Serializable {
-    private static final long serialVersionUID = 1L;
+
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
+    @NotNull
     @Column(name = "ID")
     private Integer id;
     @Basic(optional = false)
-    @Column(name = "Password")
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "NAMA")
+    private String nama;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "PASSWORD")
     private String password;
-
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "LOGINSTAT")
+    private boolean loginstat;
+    
+    private static final long serialVersionUID = 1L;
+    
     public LoginKoki() {
     }
 
@@ -90,5 +108,40 @@ public class LoginKoki implements Serializable {
     public String toString() {
         return "entity.LoginKoki[ id=" + id + " ]";
     }
+
     
+
+    public LoginKoki(Integer id, String nama, String password) {
+        this.id = id;
+        this.nama = nama;
+        this.password = password;
+    }
+
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    
+
+    public LoginKoki(Integer id, String nama, String password, boolean loginstat) {
+        this.id = id;
+        this.nama = nama;
+        this.password = password;
+        this.loginstat = loginstat;
+    }
+
+
+    public Boolean getLoginstat() {
+        return loginstat;
+    }
+
+    public void setLoginstat(boolean loginstat) {
+        this.loginstat = loginstat;
+    }
+
 }
